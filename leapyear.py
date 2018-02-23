af759461cc72f6a6b9f31ec332d022d047f1da1c
15EC048 <brindhasss.mk6@gmail.com>
2018-02-23 12:19:32 +0530

    Update leapyear.py

--- leapyear.py
+++ leapyear.py
@@ -1,5 +1,11 @@
 year=int(input())
-if(year%4==0):
- print("It is leap year")
-elif(year%4!=0):
- print("It is not leap year")
+if(year%4)==0:
+    if(year%100)==0:
+        if(year%400)==0:
+           print("It is leap year")
+        else:           
+           print("It is not leap year")
+    else:
+     print("It is not leap year")
+ else:
+print("It is leap year")
